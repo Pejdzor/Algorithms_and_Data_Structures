9c8ecc05b01859258def5a63ba662faf75aa05d8
Pejdzor <mikolajpajor@student.agh.edu.pl>
2023-04-06 22:45:23 +0200

    Offline 1

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # This is a sample Python script.
 
+<<<<<<< HEAD
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+=======
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+>>>>>>> 0c17f75 (Offline1)
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+<<<<<<< HEAD
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+=======
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+>>>>>>> 0c17f75 (Offline1)
 
 
 # Press the green button in the gutter to run the script.
